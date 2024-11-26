3c9f1b6bdfccf4e72d49e5e564006af49b884175
PhilipSteingrüber <philip.steingruber@gmail.com>
2024-11-26 11:01:01 +0100

    Toolbar component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,19 @@
-interface WorkspaceIdPageProps {
-  params: {
-    workspaceId: string;
-  };
-}
+"use client";
 
-const WorkspaceIdPage = ({ params }: WorkspaceIdPageProps) => {
-  return <div>ID: {params.workspaceId}</div>;
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+const WorkspaceIdPage = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+
+  return (
+    <>
+      <div>ID: {workspaceId}</div>
+      <div>Data: {JSON.stringify(data)}</div>
+      <div>Workspace name: {data?.name}</div>
+    </>
+  );
 };
 
 export default WorkspaceIdPage;
